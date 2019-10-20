f00c768375c6466db41eab570953501387535727
Szymon Trombik <szymon.trombik@hotmail.com>
2019-10-20 09:31:03 +0200

    added Home component

--- react-ui-crud/src/App.js
+++ react-ui-crud/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import AppHeader from './components/AppHeader';
 import AppFooter from './components/AppFooter';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return <Fragment>
       <AppHeader />
+      <Home />
       <AppFooter />
     </Fragment>;
   }
---
+++ react-ui-crud/src/components/Home.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Col, Container, Row } from 'reactstrap';
+import DataTable from './DataTable';
+import { USERS_API_URL } from '../constants';
+import { thisExpression } from '@babel/types';
+import RegistrationModal from './form/RegistrationModal';
+
+class Home extends Component {
+    state = {
+        items: []
+    }
+
+    componentDidMount() {
+        this.getItems();
+    }
+
+    getItems = () => {
+        fetch(`${USERS_API_URL}`)
+            .then(res => res.json())
+            .then(res => this.setState({ items: res }))
+            .catch(err => console.log(err));
+    }
+
+    addUserToState = user => {
+        this.setState(previous => (
+            {
+                items: [...previous.items, user]
+            }));
+    }
+
+    updateState = () => {
+        this.getItems();
+    }
+
+    deleteItemFromState = id => {
+        const updated = this.state.items.filter(item => item.id !== id);
+        this.setState({ items: updated })
+    }
+
+    render() {
+        return <Container style={{ paddingTop: "100px" }}>
+            <Row>
+                <Col>
+                    <h3>My first React + ASP.NET app</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <DataTable
+                        items={this.state.items}
+                        updateState={this.updateState}
+                        deleteItemFromState={this.deleteItemFromState} />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <RegistrationModal
+                        isNew={true}
+                        addUserToState={this.addUserToState} />
+                </Col>
+            </Row>
+        </Container>;
+    }
+}
+
+export default Home;
